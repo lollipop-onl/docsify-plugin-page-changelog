909af250bf1350155b050cdf73a3133338d64463
simochee <lollipop@simochee.net>
2023-10-25 19:19:41 +0900

    ✨ setup prettier

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const install = (hook, vm) => {
 
   if (!vm.frontmatter) {
     console.warn(
-      "[docsify-plugin-page-history] before install official front matter plugin. see https://www.npmjs.com/package/docsify-plugin-page-history#install"
+      "[docsify-plugin-page-history] before install official front matter plugin. see https://www.npmjs.com/package/docsify-plugin-page-history#install",
     );
 
     return;
