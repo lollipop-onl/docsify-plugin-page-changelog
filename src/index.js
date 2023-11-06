6aecc0b6c8a177faeeb4f68226fd6e5ecdb04c5c
simochee <lollipop@simochee.net>
2023-11-06 21:00:06 +0900

    ✨ add initial feature

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
+import htm from "htm/mini";
+import vhtml from "vhtml";
+import { parseHistory, msg } from "./utils.js";
+
+const html = htm.bind(vhtml);
+
 /** プラグインをインストール */
 const install = (hook, vm) => {
-  console.log(vm);
-
-  if (!vm.frontmatter) {
-    console.warn(
-      "[docsify-plugin-page-history] before install official front matter plugin. see https://www.npmjs.com/package/docsify-plugin-page-history#install",
-    );
-
-    return;
-  }
-
   hook.afterEach((content) => {
-    return content;
+    if (!vm.frontmatter) {
+      console.warn(
+        msg(
+          "before install official front matter plugin. see https://www.npmjs.com/package/docsify-plugin-page-history#install",
+        ),
+      );
+
+      return content;
+    }
+
+    const history = parseHistory(vm.frontmatter.history);
+    const lastModified = history[0]?.date;
+
+    const details = html`
+      <details class="plugin-page-history">
+        <summary>
+          ${lastModified ? `Last modified at ${lastModified}` : "No history"}
+        </summary>
+        ${history.length
+          ? html`
+              <dl>
+                ${history.map(
+                  ({ date, message }) => html`
+                    <dt><strong>${date}</strong></dt>
+                    <dd
+                      dangerouslySetInnerHTML=${{
+                        __html: marked(message, { breaks: true }),
+                      }}
+                    />
+                  `,
+                )}
+              </dl>
+            `
+          : html`<p>There is no history for this page.</p>`}
+      </details>
+    `;
+
+    return `${content}${details}`;
   });
 };
 
